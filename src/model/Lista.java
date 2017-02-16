def7c1bba11a7288fd60c6f6eb0b1b7666930bc1
Usuario <Usuario>
2017-02-15 23:36:16 -0600

    HackerRank

---
+++ src/model/Lista.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Lista<T extends Comparable<T>> {
+    private Nodo<T> inicio;
+    
+    public Lista(){
+        this.inicio = null;
+    }
+    
+    public void insertarInicio(Comparable<T> e) {
+        Nodo nuevo = new Nodo(e);
+        if (inicio == null) {
+            inicio = nuevo;
+        } else {
+            nuevo.setSiguiente(inicio);
+            inicio = nuevo;
+        }
+    }
+    
+    public void insertarFinal(Comparable<T> e) {
+        Nodo nuevo = new Nodo(e);
+        if (inicio == null) {
+            inicio = nuevo;
+        } else {
+            Nodo aux = inicio;
+            while(aux.getSiguiente() != null) {
+                aux = aux.getSiguiente();
+            }
+            aux.setSiguiente(nuevo);
+        }
+    }
+    
+    public void imprimir(){
+        Nodo aux = inicio;
+        while (aux != null) {
+            System.out.println(aux.getDato());
+            aux = aux.getSiguiente();
+        }
+    }
+    
+    public void insertarOrdenado(Comparable<T> e) {
+        Nodo nuevo = new Nodo(e);
+        if (inicio == null) {
+            inicio = nuevo;
+        } else {
+            Nodo aux = inicio;
+            if (aux.getDato().compareTo(nuevo.getDato())>0){
+                nuevo.setSiguiente(inicio);
+                inicio = nuevo;
+            } else {
+                Nodo anterior = null;
+                while(aux != null && nuevo.getDato().compareTo(aux.getDato())>0) {
+                    anterior = aux;
+                    aux = aux.getSiguiente();
+                }
+                nuevo.setSiguiente(aux);
+                anterior.setSiguiente(nuevo);
+            }
+        }
+    }
+    
+    public void eliminar(Comparable<T> e) {
+        Nodo aux = inicio;
+        Nodo anterior = null;
+        while(aux != null && aux.getDato().compareTo(e)!=0) {
+            anterior = aux;
+            aux = aux.getSiguiente();
+        }
+        if (aux != null) {
+            if (anterior == null) {
+                inicio = aux.getSiguiente();
+                aux.setSiguiente(null);
+            } else {
+                anterior.setSiguiente(aux.getSiguiente());
+                aux.setSiguiente(null);
+            }
+        }
+    }
+}
---
+++ src/model/Nodo.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Nodo<T extends Comparable<T>> {
+    private T dato;
+    private Nodo<T> siguiente;
+    
+    public Nodo(T dato){
+        this.dato = dato;
+        siguiente = null;
+    }
+    
+    public void print(Nodo head) { // Método HackerRank Print the elements of a linked list
+    if(head != null){
+         System.out.println(head.getDato());
+         print(head.getSiguiente());
+        }
+    }
+    public Nodo insert(Nodo head,int data) { //Método HackerRank Insert a node at the tail of a linked list
+        Nodo tail = new Nodo(data);
+        if (head == null) {
+            head = tail;
+        } else {
+            Nodo aux = head;
+            while(aux.getSiguiente() != null) {
+                aux = aux.getSiguiente();
+            }
+            aux.setSiguiente(tail);
+        }
+        return head;
+    }
+    
+    public Nodo Insert(Nodo head,int x) { //Método HackerRank Insert a node at the head of a linked list
+        Nodo nuevo = new Nodo(x);
+        if (head == null) {
+            head = nuevo;
+        } else {
+            nuevo.setSiguiente(head);
+            head = nuevo;
+        }
+        return head;
+    }
+    
+    public Nodo InsertNth(Nodo head, int data, int position) { //Método HackerRank Insert a node at a specific position in a linked list
+        Nodo nuevo = new Nodo(data);
+        if (head == null && position == 0){
+            head = nuevo;
+        } else {
+            Nodo aux = head;
+            Nodo ant = null;
+            int pos = 0;
+            while (aux != null && pos != position) {
+                ant = aux;
+                aux = aux.getSiguiente();
+                pos++;
+            }
+            if(ant == null) {
+                nuevo.setSiguiente(head);
+                head = nuevo;
+            } else {
+                nuevo.setSiguiente(aux);
+                ant.setSiguiente(nuevo);
+            }
+        }
+        return head;
+    }
+    
+    public Nodo Delete(Nodo head, int position) { //Método HackerRank Delete a node
+        Nodo aux = head;
+        Nodo ant = null;
+        int pos = 0;
+        while(aux != null && pos != position) {
+            ant = aux;
+            aux = aux.getSiguiente();
+            pos++;
+        }
+        if (aux != null) {
+            if (ant == null) {
+                head = aux.getSiguiente();
+                aux.setSiguiente(null);
+            } else {
+                ant.setSiguiente(aux.getSiguiente());
+                aux.setSiguiente(null);
+            }
+        }
+        return head;
+    }
+    
+    public void ReversePrint(Nodo head) { //Método HackerRank Print in reverse
+        if (head != null){
+            ReversePrint(head.getSiguiente());
+            System.out.println(head.getDato());
+        }
+    }
+    
+    public Nodo Reverse(Nodo head) { //Método HackerRank Reverse a linked list
+        if (head == null || head.getSiguiente() == null){
+            return head;
+        } else {
+            Nodo resto = Reverse(head.getSiguiente());
+            head.getSiguiente().setSiguiente(head);
+            head.setSiguiente(null);
+            return resto;
+        }
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+    
+}
